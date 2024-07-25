d2199ef2053cd993ededf149c27c6f77e45bfa6a
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-25 12:10:50 +0530

    SoundaryaD_extend5

--- check_limits.py
+++ check_limits.py
@@ -47,7 +47,9 @@ def battery_is_ok(temperature, soc, charge_rate):
     if warnings:
         for warning in warnings:
             print(warning)
-    return not warnings
+        return False
+    else:
+        return True
 if __name__ == '__main__':
     assert battery_is_ok(25, 70, 0.7) is True
     assert battery_is_ok(50, 85, 0) is False
